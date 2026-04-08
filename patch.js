052cca49c7d0c26ba6b6437b6ddbcd9431f67be3
kreutner <tommy.kreutner@gmail.com>
2026-04-08 21:47:38 +0200

    Add guardian phone to confirmation page

--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 ﻿const fs=require("fs");
 let c=fs.readFileSync("public/prototype.html","utf-8");
-const old = "    studentName:bookingState.studentName,studentEmail:bookingState.studentEmail,\r\n    guardianEmail:bookingState.guardianEmail,phone:bookingState.phone,";
-const newCode = "    studentName:bookingState.studentName,studentEmail:bookingState.studentEmail,\r\n    guardianEmail:bookingState.guardianEmail,phone:bookingState.phone,guardianPhone:bookingState.guardianPhone||'',";
+const old = "      <div class=\"sum-line\"><span class=\"sl\">Elevens e-post</span><span class=\"sv\">${s.studentEmail}</span></div>\r\n      <div class=\"sum-line\"><span class=\"sl\">Vård.hav. e-post</span><span class=\"sv\">${s.guardianEmail}</span></div>\r\n      <div class=\"sum-line\"><span class=\"sl\">Telefon</span><span class=\"sv\">${s.phone}</span></div>";
+const newCode = "      <div class=\"sum-line\"><span class=\"sl\">Elevens e-post</span><span class=\"sv\">${s.studentEmail}</span></div>\r\n      <div class=\"sum-line\"><span class=\"sl\">Elevens telefon</span><span class=\"sv\">${s.phone}</span></div>\r\n      <div class=\"sum-line\"><span class=\"sl\">Vård.hav. e-post</span><span class=\"sv\">${s.guardianEmail}</span></div>\r\n      <div class=\"sum-line\"><span class=\"sl\">Vård.hav. telefon</span><span class=\"sv\">${s.guardianPhone||''}</span></div>";
 c=c.replace(old,newCode);
 fs.writeFileSync("public/prototype.html",c);
-console.log("Done:", c.includes("guardianPhone"));
+console.log("Done:", c.includes("Vård.hav. telefon"));
