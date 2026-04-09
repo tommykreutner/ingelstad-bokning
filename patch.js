b593cb632c90f2bcf2ae81e5ffd5563cc63f42e0
kreutner <tommy.kreutner@gmail.com>
2026-04-10 00:11:55 +0200

    Fix buildCombos to use getSlotsForProg in all cases

--- patch.js
+++ patch.js
@@ -1,6 +1,7 @@
 ﻿const fs=require("fs");
 let c=fs.readFileSync("public/prototype.html","utf-8");
-c=c.split("' kl 15:50 (2 nätter)'").join("' (2 nätter)'");
-c=c.split("' kl 15:50'").join("'");
+const old = "        const getFullDay=(date)=>{\r\n          const avail=DB.slots.filter(s=>s.programId===pid&&s.date===date&&s.booked<s.capacity&&isDateBookable(s.date));\r\n          const hel=avail.find(s=>s.type==='hel');\r\n          if(hel) return {slots:[hel.id],label:prog.icon+' '+prog.name+' (heldag)',spots:hel.capacity-hel.booked};";
+const newCode = "        const getFullDay=(date)=>{\r\n          const avail=getSlotsForProg(date,pid);\r\n          const hel=avail.find(s=>s.type==='hel');\r\n          if(hel) return {slots:[hel.id],label:prog.icon+' '+prog.name+' (heldag)',spots:hel.capacity-hel.booked};";
+c=c.replace(old,newCode);
 fs.writeFileSync("public/prototype.html",c);
-console.log("Done:", !c.includes("kl 15:50"));
+console.log("Done:", c.includes("getSlotsForProg(date,pid);\r\n          const hel=avail.find"));
