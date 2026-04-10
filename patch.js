0c9b3846b0bb0bf3802f021f28b8aff47b20bead
kreutner <tommy.kreutner@gmail.com>
2026-04-10 08:00:59 +0200

    Fix syntax error in overnightStayLabel

--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 ﻿const fs=require("fs");
 let c=fs.readFileSync("public/prototype.html","utf-8");
-const old = "        const getFullDay=(date)=>{\r\n          const avail=DB.slots.filter(s=>s.programId===pid&&s.date===date&&s.booked<s.capacity&&isDateBookable(s.date));\r\n          const hel=avail.find(s=>s.type==='hel');\r\n          if(hel) return {slots:[hel.id],label:prog.icon+' '+prog.name+' (heldag)',spots:hel.capacity-hel.booked};";
-const newCode = "        const getFullDay=(date)=>{\r\n          const avail=getSlotsForProg(date,pid);\r\n          const hel=avail.find(s=>s.type==='hel');\r\n          if(hel) return {slots:[hel.id],label:prog.icon+' '+prog.name+' (heldag)',spots:hel.capacity-hel.booked};";
+const old = "  return 'Ankomst '+fmtDate(arrivalNight)+' kväll, avresa '+fmtDate(day1)+';";
+const newCode = "  return 'Ankomst '+fmtDate(arrivalNight)+' kväll, avresa '+fmtDate(day1);";
 c=c.replace(old,newCode);
 fs.writeFileSync("public/prototype.html",c);
-console.log("Done:", c.includes("getSlotsForProg(date,pid);\r\n          const hel=avail.find"));
+console.log("Done:", !c.includes("fmtDate(day1)+'"));
